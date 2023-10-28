b963882eb294e196604fc89fb96aea7eae3cea22
mireueden <100820682+mireueden@users.noreply.github.com>
2023-10-28 13:34:23 +0900

    10.28
    카메라 움직임 보간 적용

--- TeamByte/Assets/Resources/UI/FollowCamera.cs
+++ TeamByte/Assets/Resources/UI/FollowCamera.cs
@@ -7,12 +7,21 @@ public class FollowCamera : MonoBehaviour
 
     public Transform target; // 카메라가 따라갈 대상(플레이어)의 Transform 컴포넌트
 
+    public float lerpTime = 1.0f; // 보간에 사용될 시간
+
     void Update()
     {
         if (target != null)
         {
             // 플레이어의 위치를 따라가도록 카메라의 위치를 업데이트합니다.
-            transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+            //transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            // 타겟의 위치를 카메라의 x, y 좌표로 보간하여 새로운 위치 벡터를 생성
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            // 현재 카메라의 위치에서 타겟의 위치로 보간을 적용하여 새로운 카메라의 위치를 계산
+            transform.position = Vector3.Lerp(transform.position, targetPosition, lerpTime * Time.deltaTime);
+
         }
     }
 }
