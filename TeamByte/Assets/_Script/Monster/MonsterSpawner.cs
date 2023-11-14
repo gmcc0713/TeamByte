84e5008085ead9caa3d94d9eedff2b7c993cfe37
mireueden <a01084893912@gmail.com>
2023-11-14 14:36:32 +0900

    2023.11.14 Monster Spawn

--- TeamByte/Assets/_Script/Monster/MonsterSpawner.cs
+++ TeamByte/Assets/_Script/Monster/MonsterSpawner.cs
@@ -6,7 +6,7 @@ public class MonsterSpawner : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject[] m_Monster;
-    public Vector3[] m_spawnPos;
+    public GameObject[] m_spawnPos;
     private int count =0;
     void Start()
     {
@@ -18,7 +18,7 @@ public class MonsterSpawner : MonoBehaviour
     {
         for(int i = 0; i<c;i++)
         {
-            GameObject clone = Instantiate(m_Monster[i], m_spawnPos[i], Quaternion.identity);
+            GameObject clone = Instantiate(m_Monster[i], m_spawnPos[i].transform.position, Quaternion.identity);
             Debug.Log(clone);
             MonsterManager.Instance.MonsterInit(clone.GetComponent<Monster>());
         }
