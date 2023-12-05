7f1d0a32cc0454b6a6af4fe893746e34d12fa2a1
hyundok <gmcc0713@naver.com>
2023-12-05 14:57:54 +0900

    Revert "Revert "12.05""
    This reverts commit 399a43322b80b06d309bfcb91befb82510e352cc.

--- TeamByte/Assets/_Script/Scene/UIScript.cs
+++ TeamByte/Assets/_Script/Scene/UIScript.cs
@@ -15,6 +15,7 @@ public class UIScript : MonoBehaviour
 #if UNITY_EDITOR
         // 에디터 상에서는 Play 모드를 중지
         UnityEditor.EditorApplication.isPlaying = false;
+
 #else
         // 빌드된 런타임에서는 애플리케이션을 종료합니다.
         Application.Quit();
@@ -26,5 +27,8 @@ public class UIScript : MonoBehaviour
     {
         SceneManager.LoadScene("TitleScene");
     }
-
+    public void SavePoint(int save)
+    {
+        SaveLoadManager.Instance.SetSaveData(save);
+    }
 }
